dac8be3433e6621255aa428cc57c15d4addc0e5b
akourtis98 <akourtisdev@gmail.com>
2019-09-03 01:41:23 +0300

    basic functionality done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { stringify } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoangular';
+  private todoitems: any[];
+  private input: string;
+  private index: number;
+
+  constructor() {
+    this.todoitems = [];
+    this.input = "";
+    this.index = 0;
+  }
+
+  addItem() {
+    this.index++;
+
+    this.todoitems.push({ item: this.input, index: this.index })
+
+    this.input = "";
+  }
+
+  removeItem(item: any) {
+    let removeIndex = this.todoitems.map(function (item) { return item.index; }).indexOf(item.index);
+
+    this.todoitems.splice(removeIndex, 1);
+  }
 }
