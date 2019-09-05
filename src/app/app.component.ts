6a027227720e54e0712c03fb43759a7ebe232f76
akourtis98 <akourtisdev@gmail.com>
2019-09-05 22:09:39 +0300

    10:09

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { stringify } from '@angular/compiler/src/util';
+import { Component, ElementRef, ViewChild, Inject } from '@angular/core';
+import { MatDialog, MatDialogConfig, MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { inject } from '@angular/core/testing';
 
 @Component({
   selector: 'app-root',
@@ -7,31 +9,98 @@ import { stringify } from '@angular/compiler/src/util';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('newItemInput', { static: false, }) inputEl: ElementRef;
+
   private todoitems: any[];
   private input: string;
   private index: number;
   private error: string;
+  private mode: string;
+  private itemText: string;
+  private chosenItemIndex: number;
 
-  constructor() {
+  constructor(private dialog: MatDialog,
+    private snackbar: MatSnackBar) {
+
     this.todoitems = [];
     this.input = "";
     this.index = 0;
     this.error = "";
+    this.mode = "view";
+    this.itemText = "";
+  }
+
+  log(msg: string) {
+    this.snackbar.open(msg, "x", { duration: 2500 });
+    console.log("msg: ", msg);
   }
 
   addItem() {
     if (this.input.trim() != "") {
       this.todoitems.push({ item: this.input, index: ++this.index })
-      this.error = "";
       this.input = "";
     } else {
-      this.error = "Please Add A Task!"
+      this.log("Please add a task");
     }
   }
 
   removeItem(item: any) {
-    let removeIndex = this.todoitems.map(function (item) { return item.index; }).indexOf(item.index);
+    const dialogRef = this.dialog.open(DeleteConfirmation, {
+      width: '600px',
+      data: item.item
+    });
 
-    this.todoitems.splice(removeIndex, 1);
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        let removeIndex = this.todoitems.map(function (item) { return item.index; }).indexOf(item.index);
+        this.todoitems.splice(removeIndex, 1);
+      }
+    });
+  }
+
+  editItem(i: number) {
+    this.mode = "edit";
+    this.chosenItemIndex = i;
+    this.itemText = this.todoitems[i].item;
+
+    setTimeout(() => this.inputEl.nativeElement.focus(), 0);
+  }
+
+  saveItem(index: number) {
+    if (this.itemText) {
+      this.todoitems[index].item = this.itemText;
+      this.cancel();
+    } else {
+      this.log("Must not be empty");
+    }
+  }
+
+  cancel() {
+    this.itemText = "";
+    this.mode = "view";
+    this.chosenItemIndex = null;
+  }
+}
+
+
+@Component({
+  selector: 'delete-confirmation',
+  templateUrl: './delete-confirmation.component.html'
+})
+export class DeleteConfirmation {
+
+  private taskname: string;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: string,
+    public dialogRef: MatDialogRef<DeleteConfirmation>) {
+    this.taskname = data;
+  }
+
+  no() {
+    this.dialogRef.close(false);
+  }
+
+  ok() {
+    this.dialogRef.close(true);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { DeleteConfirmation } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
+    MatDialogModule,
     BrowserModule,
     AppRoutingModule,
+    MatSnackBarModule,
+    BrowserAnimationsModule,
     FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    DeleteConfirmation
+  ],
+  entryComponents: [
+    DeleteConfirmation
   ],
   providers: [],
   bootstrap: [AppComponent]
